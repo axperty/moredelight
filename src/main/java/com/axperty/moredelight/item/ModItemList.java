7627120195e5eecd7d6ccea9ec76d24abd01bba1
axperty <kevingelhorn0@gmail.com>
2024-09-16 17:51:41 -0300

    Adjusted hunger and saturation for certain food items, changed and removed effects, created a new class ModEffectToolTip to display effect duration times.

---
+++ src/main/java/com/axperty/moredelight/item/ModItemList.java
@@ -0,0 +1,75 @@
+package com.axperty.moredelight.item;
+
+import net.minecraft.item.Item;
+
+public class ModItemList {
+    public static Item WOODEN_KNIFE;
+
+    public static Item STONE_KNIFE;
+
+    public static Item DICED_POTATOES;
+
+    public static Item CHOCOLATE_POPSICLE;
+
+    public static Item OMELETTE;
+
+    public static Item COOKED_RICE_WITH_MILK_CREAM_AND_CHICKEN_CUTS;
+
+    public static Item COOKED_RICE_WITH_MILK_CREAM_AND_BEEF;
+
+    public static Item COOKED_RICE_WITH_MILK_CREAM_AND_PORKCHOP;
+
+    public static Item MASHED_POTATOES;
+
+    public static Item PASTA_WITH_MILK_CREAM_AND_HAM;
+
+    public static Item COOKED_DICED_POTATOES_WITH_CHICKEN_CUTS;
+
+    public static Item COOKED_DICED_POTATOES_WITH_BEEF;
+
+    public static Item COOKED_DICED_POTATOES_WITH_PORKCHOP;
+
+    public static Item CHICKEN_SALAD;
+
+    public static Item POTATO_SALAD;
+
+    public static Item CARROT_SOUP;
+
+    public static Item SIMPLE_HAMBURGER;
+
+    public static Item HAMBURGER_WITH_CHEESE;
+
+    public static Item HAMBURGER_WITH_EGG;
+
+    public static Item LOADED_HAMBURGER;
+
+    public static Item CHICKEN_SANDWICH_WITH_EGG_AND_TOMATO;
+
+    public static Item STEAK_SANDWICH;
+
+    public static Item PORKCHOP_SANDWICH;
+
+    public static Item EGG_WITH_BACON_SANDWICH;
+
+    public static Item TOMATO_SANDWICH;
+
+    public static Item BREAD_SLICE;
+
+    public static Item TOAST;
+
+    public static Item TOAST_WITH_EGG;
+
+    public static Item TOAST_WITH_HONEY;
+
+    public static Item TOAST_WITH_SWEET_BERRIES;
+
+    public static Item TOAST_WITH_BLUEBERRIES;
+
+    public static Item TOAST_WITH_GLOW_BERRIES;
+
+    public static Item TOAST_WITH_CHOCOLATE;
+
+    public static Item TOAST_WITH_CHEESE;
+
+    public static Item TOAST_WITH_PEANUT_BUTTER;
+}
--- src/main/java/com/axperty/moredelight/item/ModItems.java
+++ src/main/java/com/axperty/moredelight/item/ModItems.java
@@ -22,100 +22,100 @@ public class ModItems {
             new KnifeItem(ModMaterials.STONE_MATERIAL, new FabricItemSettings().group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item DICED_POTATOES = registerItem("diced_potatoes",
-            new Item(food(null, 1, 0.3f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
-
-    public static final Item OMELETTE = registerItem("omelette",
-            new Item(food(null, 6, .5f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 2, 0.4f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item CHOCOLATE_POPSICLE = registerItem("chocolate_popsicle",
             new Item(food(null, 3, 0.2f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
+    public static final Item OMELETTE = registerItem("omelette",
+            new Item(food(null, 6, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+
     public static final Item COOKED_RICE_WITH_MILK_CREAM_AND_CHICKEN_CUTS = registerItem("cooked_rice_with_milk_cream_and_chicken_cuts",
-            new Item(meal(Items.BOWL,10, .75f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            foodEffect(Items.BOWL, EffectsRegistry.NOURISHMENT.get(), 3600, 0, 14, 0.8f));
 
     public static final Item COOKED_RICE_WITH_MILK_CREAM_AND_BEEF = registerItem("cooked_rice_with_milk_cream_and_beef",
-            new Item(meal(Items.BOWL,10, .75f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            foodEffect(Items.BOWL, EffectsRegistry.NOURISHMENT.get(), 3600, 0, 14, 0.8f));
 
     public static final Item COOKED_RICE_WITH_MILK_CREAM_AND_PORKCHOP = registerItem("cooked_rice_with_milk_cream_and_porkchop",
-            new Item(meal(Items.BOWL,10, .75f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            foodEffect(Items.BOWL, EffectsRegistry.NOURISHMENT.get(), 3600, 0, 14, 0.8f));
 
     public static final Item MASHED_POTATOES = registerItem("mashed_potatoes",
-            new Item(food(Items.BOWL,10, 0.75f).maxCount(16).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(Items.BOWL,12, 0.8f).maxCount(16).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item PASTA_WITH_MILK_CREAM_AND_HAM = registerItem("pasta_with_milk_cream_and_ham",
-            new Item(meal(Items.BOWL,10, 0.75f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            foodEffect(Items.BOWL, EffectsRegistry.NOURISHMENT.get(), 3600, 0, 12, 0.8f));
 
     public static final Item COOKED_DICED_POTATOES_WITH_CHICKEN_CUTS = registerItem("cooked_diced_potatoes_with_chicken_cuts",
-            new Item(foodEffect(Items.BOWL, StatusEffects.SPEED, 200, 0, 10, .75f).maxCount(16).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(Items.BOWL, 10, 0.8f)));
 
     public static final Item COOKED_DICED_POTATOES_WITH_BEEF = registerItem("cooked_diced_potatoes_with_beef",
-            new Item(foodEffect(Items.BOWL, StatusEffects.STRENGTH, 200, 0, 10, .75f).maxCount(16).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(Items.BOWL, 10, 0.8f)));
 
     public static final Item COOKED_DICED_POTATOES_WITH_PORKCHOP = registerItem("cooked_diced_potatoes_with_porkchop",
-            new Item(foodEffect(Items.BOWL, StatusEffects.RESISTANCE, 200, 0, 10, .75f).maxCount(16).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(Items.BOWL, 10, 0.8f)));
 
     public static final Item CHICKEN_SALAD = registerItem("chicken_salad",
-            new Item(food(Items.BOWL,6, 0.6f).maxCount(16).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            foodEffect(Items.BOWL, StatusEffects.REGENERATION, 100, 0, 6, 0.6f));
 
     public static final Item POTATO_SALAD = registerItem("potato_salad",
-            new Item(food(Items.BOWL,6, 0.6f).maxCount(16).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            foodEffect(Items.BOWL, StatusEffects.REGENERATION, 100, 0, 6, 0.6f));
 
     public static final Item CARROT_SOUP = registerItem("carrot_soup",
-            new Item(stew(9, 0.75f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            foodEffect(Items.BOWL, EffectsRegistry.COMFORT.get(), 3600, 0, 12, 0.8f));
 
     public static final Item SIMPLE_HAMBURGER = registerItem("simple_hamburger",
-            new Item(food(null, 5, 0.4f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 8, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item HAMBURGER_WITH_CHEESE = FabricLoader.getInstance().isModLoaded("expandeddelight") || FabricLoader.getInstance().isModLoaded("casualness_delight") ?
-            registerItem("hamburger_with_cheese", new Item(food(null, 6, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB))): null;
+            registerItem("hamburger_with_cheese", new Item(food(null, 9, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB))): null;
 
     public static final Item HAMBURGER_WITH_EGG = registerItem("hamburger_with_egg",
-            new Item(food(null, 7, 0.7f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 9, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item LOADED_HAMBURGER = registerItem("loaded_hamburger",
-            new Item(food(null, 15, 1f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 13, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item CHICKEN_SANDWICH_WITH_EGG_AND_TOMATO = registerItem("chicken_sandwich_with_egg_and_tomato",
-            new Item(food(null, 11, 0.9f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 11, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item STEAK_SANDWICH = registerItem("steak_sandwich",
-            new Item(food(null, 7, 0.7f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
-
-    public static final Item PORKCHOP_SANDWICH = registerItem("porkchop_sandwich",
-            new Item(food(null, 7, 0.7f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
-
-    public static final Item EGG_WITH_BACON_SANDWICH = registerItem("egg_with_bacon_sandwich",
             new Item(food(null, 10, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
+    public static final Item PORKCHOP_SANDWICH = registerItem("porkchop_sandwich",
+            new Item(food(null, 10, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+
+    public static final Item EGG_WITH_BACON_SANDWICH = registerItem("egg_with_bacon_sandwich",
+            new Item(food(null, 11, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+
     public static final Item TOMATO_SANDWICH = registerItem("tomato_sandwich",
-            new Item(food(null, 6, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 7, 0.8f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item BREAD_SLICE = registerItem("bread_slice",
             new Item(food(null, 2, 0.4f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item TOAST = registerItem("toast",
-            new Item(food(null, 2, 0.4f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 3, 0.4f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item TOAST_WITH_EGG = registerItem("toast_with_egg",
-            new Item(food(null, 5, 0.5f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 5, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item TOAST_WITH_HONEY = registerItem("toast_with_honey",
-            new Item(food(null, 5, 0.5f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 5, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item TOAST_WITH_SWEET_BERRIES = registerItem("toast_with_sweet_berries",
-            new Item(food(null, 5, 0.5f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 5, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item TOAST_WITH_GLOW_BERRIES = registerItem("toast_with_glow_berries",
-            new Item(foodEffect(null, StatusEffects.GLOWING, 200, 0, 5, .5f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 5, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item TOAST_WITH_CHOCOLATE = registerItem("toast_with_chocolate",
-            new Item(food(null, 5, 0.5f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
+            new Item(food(null, 5, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB)));
 
     public static final Item TOAST_WITH_CHEESE = FabricLoader.getInstance().isModLoaded("expandeddelight") || FabricLoader.getInstance().isModLoaded("casualness_delight") ?
-            registerItem("toast_with_cheese", new Item(food(null, 5, 0.5f).group(ModCreativeTab.CREATIVE_MODE_TAB))): null;
+            registerItem("toast_with_cheese", new Item(food(null, 5, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB))): null;
 
     public static final Item TOAST_WITH_PEANUT_BUTTER = FabricLoader.getInstance().isModLoaded("expandeddelight") ?
-            registerItem("toast_with_peanut_butter", new Item(food(null, 5, 0.5f).group(ModCreativeTab.CREATIVE_MODE_TAB))): null;
+            registerItem("toast_with_peanut_butter", new Item(food(null, 5, 0.6f).group(ModCreativeTab.CREATIVE_MODE_TAB))): null;
 
     private static FabricItemSettings food(Item remainder, int hunger, float saturation) {
         return new FabricItemSettings()
@@ -126,14 +126,14 @@ public class ModItems {
                         .build());
     }
 
-    private static FabricItemSettings foodEffect(Item remainder, StatusEffect effect, int duration, int amplifier, int hunger, float saturation) {
-        return new FabricItemSettings()
+    private static ModEffectToolTip foodEffect(Item remainder, StatusEffect effect, int duration, int amplifier, int hunger, float saturation) {
+        return new ModEffectToolTip(effect, duration, amplifier, new FabricItemSettings()
                 .recipeRemainder(remainder)
                 .food(new FoodComponent.Builder()
                         .hunger(hunger)
                         .saturationModifier(saturation)
-                        .statusEffect(new StatusEffectInstance(effect, duration, amplifier), 1.0f)
-                        .build());
+                        .build())
+        .group(ModCreativeTab.CREATIVE_MODE_TAB));
     }
 
     private static FabricItemSettings meal(Item remainder, int hunger, float saturation) {
