8b3526665d739480c8b61877058c847a1416deec
axperty <kevingelhorn0@gmail.com>
2024-09-16 15:37:16 -0300

    Adjusted hunger and saturation for certain food items, changed and removed effects, created a new class ModEffectToolTip to display effect duration times.

--- src/main/java/com/axperty/moredelight/item/ModItems.java
+++ src/main/java/com/axperty/moredelight/item/ModItems.java
@@ -37,15 +37,15 @@ public class ModItems {
 
     public static final RegistryObject<Item> DICED_POTATOES = ITEMS.register("diced_potatoes",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(1)
-                    .saturationMod(0.3f)
+                    .nutrition(2)
+                    .saturationMod(0.4f)
                     .fast()
                     .build())));
 
     public static final RegistryObject<Item> OMELETTE = ITEMS.register("omelette",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(6)
-                    .saturationMod(0.5f)
+                    .saturationMod(0.6f)
                     .build())));
 
     public static final RegistryObject<Item> CHOCOLATE_POPSICLE = ITEMS.register("chocolate_popsicle",
@@ -56,133 +56,129 @@ public class ModItems {
 
     public static final RegistryObject<Item> COOKED_RICE_WITH_MILK_CREAM_AND_CHICKEN_CUTS = ITEMS.register("cooked_rice_with_milk_cream_and_chicken_cuts",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
-                    .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
+                    .nutrition(14)
+                    .saturationMod(0.8f)
+                    .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT.get(), FoodValues.MEDIUM_DURATION, 0), 1.0F)
                     .build()), true));
 
     public static final RegistryObject<Item> COOKED_RICE_WITH_MILK_CREAM_AND_BEEF = ITEMS.register("cooked_rice_with_milk_cream_and_beef",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
-                    .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
+                    .nutrition(14)
+                    .saturationMod(0.8f)
+                    .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT.get(), FoodValues.MEDIUM_DURATION, 0), 1.0F)
                     .build()), true));
 
     public static final RegistryObject<Item> COOKED_RICE_WITH_MILK_CREAM_AND_PORKCHOP = ITEMS.register("cooked_rice_with_milk_cream_and_porkchop",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
-                    .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
+                    .nutrition(14)
+                    .saturationMod(0.8f)
+                    .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT.get(), FoodValues.MEDIUM_DURATION, 0), 1.0F)
                     .build()), true));
 
     public static final RegistryObject<Item> MASHED_POTATOES = ITEMS.register("mashed_potatoes",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
-                    .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
-                    .build()), true));
+                    .nutrition(12)
+                    .saturationMod(0.8f)
+                    .build())));
 
     public static final RegistryObject<Item> PASTA_WITH_MILK_CREAM_AND_HAM = ITEMS.register("pasta_with_milk_cream_and_ham",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
-                    .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
+                    .nutrition(12)
+                    .saturationMod(0.8f)
+                    .effect(() -> new MobEffectInstance(ModEffects.NOURISHMENT.get(), FoodValues.MEDIUM_DURATION, 0), 1.0F)
                     .build()), true));
 
     public static final RegistryObject<Item> COOKED_DICED_POTATOES_WITH_CHICKEN_CUTS = ITEMS.register("cooked_diced_potatoes_with_chicken_cuts",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
                     .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 0), 1.0F)
-                    .build()), true));
+                    .saturationMod(0.8f)
+                    .build())));
 
     public static final RegistryObject<Item> COOKED_DICED_POTATOES_WITH_BEEF = ITEMS.register("cooked_diced_potatoes_with_beef",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
                     .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 0), 1.0F)
-                    .build()), true));
+                    .saturationMod(0.8f)
+                    .build())));
 
     public static final RegistryObject<Item> COOKED_DICED_POTATOES_WITH_PORKCHOP = ITEMS.register("cooked_diced_potatoes_with_porkchop",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
                     .nutrition(10)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 200, 0), 1.0F)
-                    .build()), true));
+                    .saturationMod(0.8f)
+                    .build())));
 
     public static final RegistryObject<Item> CHICKEN_SALAD = ITEMS.register("chicken_salad",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
                     .nutrition(6)
                     .saturationMod(0.6f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
+                    .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 100, 0), 1.0F)
                     .build()), true));
 
     public static final RegistryObject<Item> POTATO_SALAD = ITEMS.register("potato_salad",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
                     .nutrition(6)
                     .saturationMod(0.6f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
+                    .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 100, 0), 1.0F)
                     .build()), true));
 
     public static final RegistryObject<Item> CARROT_SOUP = ITEMS.register("carrot_soup",
             () -> new ConsumableItem(bowlFoodItem((new FoodProperties.Builder())
-                    .nutrition(9)
-                    .saturationMod(0.75f)
-                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.SHORT_DURATION, 0), 1.0F)
+                    .nutrition(12)
+                    .saturationMod(0.8f)
+                    .effect(() -> new MobEffectInstance(ModEffects.COMFORT.get(), FoodValues.MEDIUM_DURATION, 0), 1.0F)
                     .build()), true));
 
     public static final RegistryObject<Item> SIMPLE_HAMBURGER = ITEMS.register("simple_hamburger",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(5)
-                    .saturationMod(0.4f)
+                    .nutrition(8)
+                    .saturationMod(0.8f)
                     .build())));
 
     public static final RegistryObject<Item> HAMBURGER_WITH_CHEESE = ITEMS.register("hamburger_with_cheese",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(6)
-                    .saturationMod(0.6f)
+                    .nutrition(9)
+                    .saturationMod(0.8f)
                     .build())));
 
     public static final RegistryObject<Item> HAMBURGER_WITH_EGG = ITEMS.register("hamburger_with_egg",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(7)
-                    .saturationMod(0.7f)
+                    .nutrition(9)
+                    .saturationMod(0.8f)
                     .build())));
 
     public static final RegistryObject<Item> LOADED_HAMBURGER = ITEMS.register("loaded_hamburger",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(15)
-                    .saturationMod(1f)
+                    .nutrition(13)
+                    .saturationMod(0.8f)
                     .build())));
 
     public static final RegistryObject<Item> CHICKEN_SANDWICH_WITH_EGG_AND_TOMATO = ITEMS.register("chicken_sandwich_with_egg_and_tomato",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(11)
-                    .saturationMod(0.9f)
+                    .saturationMod(0.8f)
                     .build())));
 
     public static final RegistryObject<Item> STEAK_SANDWICH = ITEMS.register("steak_sandwich",
-            () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(7)
-                    .saturationMod(0.7f)
-                    .build())));
-
-    public static final RegistryObject<Item> PORKCHOP_SANDWICH = ITEMS.register("porkchop_sandwich",
-            () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(7)
-                    .saturationMod(0.7f)
-                    .build())));
-
-    public static final RegistryObject<Item> EGG_WITH_BACON_SANDWICH = ITEMS.register("egg_with_bacon_sandwich",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(10)
                     .saturationMod(0.8f)
                     .build())));
 
+    public static final RegistryObject<Item> PORKCHOP_SANDWICH = ITEMS.register("porkchop_sandwich",
+            () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
+                    .nutrition(10)
+                    .saturationMod(0.8f)
+                    .build())));
+
+    public static final RegistryObject<Item> EGG_WITH_BACON_SANDWICH = ITEMS.register("egg_with_bacon_sandwich",
+            () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
+                    .nutrition(11)
+                    .saturationMod(0.8f)
+                    .build())));
+
     public static final RegistryObject<Item> TOMATO_SANDWICH = ITEMS.register("tomato_sandwich",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(6)
-                    .saturationMod(0.6f)
+                    .nutrition(7)
+                    .saturationMod(0.8f)
                     .build())));
 
     public static final RegistryObject<Item> BREAD_SLICE = ITEMS.register("bread_slice",
@@ -193,50 +189,49 @@ public class ModItems {
 
     public static final RegistryObject<Item> TOAST = ITEMS.register("toast",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
-                    .nutrition(2)
+                    .nutrition(3)
                     .saturationMod(0.4f)
                     .build())));
 
     public static final RegistryObject<Item> TOAST_WITH_EGG = ITEMS.register("toast_with_egg",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(5)
-                    .saturationMod(0.5f)
+                    .saturationMod(0.6f)
                     .build())));
 
     public static final RegistryObject<Item> TOAST_WITH_HONEY = ITEMS.register("toast_with_honey",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(5)
-                    .saturationMod(0.5f)
+                    .saturationMod(0.6f)
                     .build())));
 
     public static final RegistryObject<Item> TOAST_WITH_SWEET_BERRIES = ITEMS.register("toast_with_sweet_berries",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(5)
-                    .saturationMod(0.5f)
+                    .saturationMod(0.6f)
                     .build())));
 
     public static final RegistryObject<Item> TOAST_WITH_BLUEBERRIES =  ITEMS.register("toast_with_blueberries",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(5)
-                    .saturationMod(0.5f)
+                    .saturationMod(0.6f)
                     .build())));
 
     public static final RegistryObject<Item> TOAST_WITH_GLOW_BERRIES = ITEMS.register("toast_with_glow_berries",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(5)
-                    .saturationMod(0.5f)
-                    .effect(() -> new MobEffectInstance(MobEffects.GLOWING, 200, 0), 1.0f)
-                    .build()), true));
+                    .saturationMod(0.6f)
+                    .build())));
 
     public static final RegistryObject<Item> TOAST_WITH_CHOCOLATE = ITEMS.register("toast_with_chocolate",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(5)
-                    .saturationMod(0.5f)
+                    .saturationMod(0.6f)
                     .build())));
 
     public static final RegistryObject<Item> TOAST_WITH_CHEESE = ITEMS.register("toast_with_cheese",
             () -> new ConsumableItem(foodItem((new FoodProperties.Builder())
                     .nutrition(5)
-                    .saturationMod(0.5f)
+                    .saturationMod(0.6f)
                     .build())));
 }
