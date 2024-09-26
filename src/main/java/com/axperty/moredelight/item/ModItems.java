f209c115b2069c80d03d730230e8cbab4ebc8fa6
axperty <kevingelhorn0@gmail.com>
2024-09-25 23:46:43 -0300

    Fixed a bug where the bowl item wasn't returned after eating a meal, and other code improvements.

--- src/main/java/com/axperty/moredelight/item/ModItems.java
+++ src/main/java/com/axperty/moredelight/item/ModItems.java
@@ -22,78 +22,78 @@ public class ModItems {
 
         ModItemList.STONE_KNIFE = knife("stone_knife", new KnifeItem(ModMaterials.STONE_MATERIAL, new FabricItemSettings()));
 
-        ModItemList.DICED_POTATOES = consumable("diced_potatoes", 2, 0.4f, 64);
+        ModItemList.DICED_POTATOES = consumable("diced_potatoes", 2, 0.4f, 64, null);
 
-        ModItemList.CHOCOLATE_POPSICLE = consumable("chocolate_popsicle", 3, 0.2f, 64);
+        ModItemList.CHOCOLATE_POPSICLE = consumable("chocolate_popsicle", 3, 0.2f, 64, null);
 
-        ModItemList.OMELETTE = consumable("omelette", 6, 0.6f, 64);
+        ModItemList.OMELETTE = consumable("omelette", 6, 0.6f, 64, null);
 
-        ModItemList.COOKED_RICE_WITH_MILK_CREAM_AND_CHICKEN_CUTS = consumableEffect("cooked_rice_with_milk_cream_and_chicken_cuts", 14, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16);
+        ModItemList.COOKED_RICE_WITH_MILK_CREAM_AND_CHICKEN_CUTS = consumableEffect("cooked_rice_with_milk_cream_and_chicken_cuts", 14, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16, Items.BOWL);
 
-        ModItemList.COOKED_RICE_WITH_MILK_CREAM_AND_BEEF = consumableEffect("cooked_rice_with_milk_cream_and_beef", 14, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16);
+        ModItemList.COOKED_RICE_WITH_MILK_CREAM_AND_BEEF = consumableEffect("cooked_rice_with_milk_cream_and_beef", 14, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16, Items.BOWL);
 
-        ModItemList.COOKED_RICE_WITH_MILK_CREAM_AND_PORKCHOP = consumableEffect("cooked_rice_with_milk_cream_and_porkchop", 14, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16);
+        ModItemList.COOKED_RICE_WITH_MILK_CREAM_AND_PORKCHOP = consumableEffect("cooked_rice_with_milk_cream_and_porkchop", 14, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16, Items.BOWL);
 
-        ModItemList.MASHED_POTATOES = consumable("mashed_potatoes", 12, 0.8f, 16);
+        ModItemList.MASHED_POTATOES = consumable("mashed_potatoes", 12, 0.8f, 16, Items.BOWL);
 
-        ModItemList.PASTA_WITH_MILK_CREAM_AND_HAM = consumableEffect("pasta_with_milk_cream_and_ham", 12, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16);
+        ModItemList.PASTA_WITH_MILK_CREAM_AND_HAM = consumableEffect("pasta_with_milk_cream_and_ham", 12, 0.8f, ModEffects.NOURISHMENT.get(), 3600, 0, 16, Items.BOWL);
 
-        ModItemList.COOKED_DICED_POTATOES_WITH_CHICKEN_CUTS = consumable("cooked_diced_potatoes_with_chicken_cuts", 10, 0.8f, 16);
+        ModItemList.COOKED_DICED_POTATOES_WITH_CHICKEN_CUTS = consumable("cooked_diced_potatoes_with_chicken_cuts", 10, 0.8f, 16, Items.BOWL);
 
-        ModItemList.COOKED_DICED_POTATOES_WITH_BEEF = consumable("cooked_diced_potatoes_with_beef", 10, 0.8f, 16);
+        ModItemList.COOKED_DICED_POTATOES_WITH_BEEF = consumable("cooked_diced_potatoes_with_beef", 10, 0.8f, 16, Items.BOWL);
 
-        ModItemList.COOKED_DICED_POTATOES_WITH_PORKCHOP = consumable("cooked_diced_potatoes_with_porkchop", 10, 0.8f, 16);
+        ModItemList.COOKED_DICED_POTATOES_WITH_PORKCHOP = consumable("cooked_diced_potatoes_with_porkchop", 10, 0.8f, 16, Items.BOWL);
 
-        ModItemList.CHICKEN_SALAD = consumableEffect("chicken_salad", 6, 0.6f, StatusEffects.REGENERATION, 100, 0, 16);
+        ModItemList.CHICKEN_SALAD = consumableEffect("chicken_salad", 6, 0.6f, StatusEffects.REGENERATION, 100, 0, 16, Items.BOWL);
 
-        ModItemList.POTATO_SALAD = consumableEffect("potato_salad", 6, 0.6f, StatusEffects.REGENERATION, 100, 0, 16);
+        ModItemList.POTATO_SALAD = consumableEffect("potato_salad", 6, 0.6f, StatusEffects.REGENERATION, 100, 0, 16, Items.BOWL);
 
-        ModItemList.CARROT_SOUP = consumableEffect("carrot_soup", 12, 0.8f, ModEffects.COMFORT.get(), 3600, 0, 16);
+        ModItemList.CARROT_SOUP = consumableEffect("carrot_soup", 12, 0.8f, ModEffects.COMFORT.get(), 3600, 0, 16, Items.BOWL);
 
-        ModItemList.SIMPLE_HAMBURGER = consumable("simple_hamburger", 8, 0.8f, 64);
+        ModItemList.SIMPLE_HAMBURGER = consumable("simple_hamburger", 8, 0.8f, 64, null);
 
         if (FabricLoader.getInstance().isModLoaded("expandeddelight") || FabricLoader.getInstance().isModLoaded("casualness_delight")) {
-            ModItemList.HAMBURGER_WITH_CHEESE = consumable("hamburger_with_cheese", 9, 0.8f, 64);
+            ModItemList.HAMBURGER_WITH_CHEESE = consumable("hamburger_with_cheese", 9, 0.8f, 64, null);
         }
 
-        ModItemList.HAMBURGER_WITH_EGG = consumable("hamburger_with_egg", 9, 0.8f, 64);
+        ModItemList.HAMBURGER_WITH_EGG = consumable("hamburger_with_egg", 9, 0.8f, 64, null);
 
-        ModItemList.LOADED_HAMBURGER = consumable("loaded_hamburger", 13, 0.8f, 64);
+        ModItemList.LOADED_HAMBURGER = consumable("loaded_hamburger", 13, 0.8f, 64, null);
 
-        ModItemList.CHICKEN_SANDWICH_WITH_EGG_AND_TOMATO = consumable("chicken_sandwich_with_egg_and_tomato", 11, 0.8f, 64);
+        ModItemList.CHICKEN_SANDWICH_WITH_EGG_AND_TOMATO = consumable("chicken_sandwich_with_egg_and_tomato", 11, 0.8f, 64, null);
 
-        ModItemList.STEAK_SANDWICH = consumable("steak_sandwich", 10, 0.8f, 64);
+        ModItemList.STEAK_SANDWICH = consumable("steak_sandwich", 10, 0.8f, 64, null);
 
-        ModItemList.PORKCHOP_SANDWICH = consumable("porkchop_sandwich", 10, 0.8f, 64);
+        ModItemList.PORKCHOP_SANDWICH = consumable("porkchop_sandwich", 10, 0.8f, 64, null);
 
-        ModItemList.EGG_WITH_BACON_SANDWICH = consumable("egg_with_bacon_sandwich", 11, 0.8f, 64);
+        ModItemList.EGG_WITH_BACON_SANDWICH = consumable("egg_with_bacon_sandwich", 11, 0.8f, 64, null);
 
-        ModItemList.TOMATO_SANDWICH = consumable("tomato_sandwich", 7, 0.8f, 64);
+        ModItemList.TOMATO_SANDWICH = consumable("tomato_sandwich", 7, 0.8f, 64, null);
 
-        ModItemList.BREAD_SLICE = consumable("bread_slice", 2, 0.4f, 64);
+        ModItemList.BREAD_SLICE = consumable("bread_slice", 2, 0.4f, 64, null);
 
-        ModItemList.TOAST = consumable("toast", 3, 0.4f, 64);
+        ModItemList.TOAST = consumable("toast", 3, 0.4f, 64, null);
 
-        ModItemList.TOAST_WITH_EGG = consumable("toast_with_egg", 5, 0.6f, 64);
+        ModItemList.TOAST_WITH_EGG = consumable("toast_with_egg", 5, 0.6f, 64, null);
 
-        ModItemList.TOAST_WITH_HONEY = consumable("toast_with_honey", 5, 0.6f, 64);
+        ModItemList.TOAST_WITH_HONEY = consumable("toast_with_honey", 5, 0.6f, 64, null);
 
-        ModItemList.TOAST_WITH_SWEET_BERRIES = consumable("toast_with_sweet_berries", 5, 0.6f, 64);
+        ModItemList.TOAST_WITH_SWEET_BERRIES = consumable("toast_with_sweet_berries", 5, 0.6f, 64, null);
 
         if (FabricLoader.getInstance().isModLoaded("nutritious-feast")) {
-            ModItemList.TOAST_WITH_BLUEBERRIES = consumable("toast_with_blueberries", 5, 0.6f, 64);
+            ModItemList.TOAST_WITH_BLUEBERRIES = consumable("toast_with_blueberries", 5, 0.6f, 64, null);
         }
 
-        ModItemList.TOAST_WITH_GLOW_BERRIES = consumable("toast_with_glow_berries", 5, 0.6f, 64);
+        ModItemList.TOAST_WITH_GLOW_BERRIES = consumable("toast_with_glow_berries", 5, 0.6f, 64, null);
 
-        ModItemList.TOAST_WITH_CHOCOLATE = consumable("toast_with_chocolate", 5, 0.6f, 64);
+        ModItemList.TOAST_WITH_CHOCOLATE = consumable("toast_with_chocolate", 5, 0.6f, 64, null);
 
         if (FabricLoader.getInstance().isModLoaded("expandeddelight") || FabricLoader.getInstance().isModLoaded("casualness_delight")) {
-            ModItemList.TOAST_WITH_CHEESE = consumable("toast_with_cheese", 5, 0.6f, 64);
+            ModItemList.TOAST_WITH_CHEESE = consumable("toast_with_cheese", 5, 0.6f, 64, null);
         }
 
         if (FabricLoader.getInstance().isModLoaded("expandeddelight")) {
-            ModItemList.TOAST_WITH_PEANUT_BUTTER = consumable("toast_with_peanut_butter", 5, 0.6f, 64);
+            ModItemList.TOAST_WITH_PEANUT_BUTTER = consumable("toast_with_peanut_butter", 5, 0.6f, 64, null);
         }
     }
 
@@ -103,9 +103,10 @@ public class ModItems {
         return item;
     }
 
-    private static Item consumable(String name, int hunger, float saturation, int maxCount) {
+    private static Item consumable(String name, int hunger, float saturation, int maxCount, Item remainder) {
         Item item = Registry.register(Registries.ITEM, new Identifier(MoreDelight.MOD_ID, name),
                 new ConsumableItem(new FabricItemSettings()
+                        .recipeRemainder(remainder)
                         .maxCount(maxCount)
                         .food(new FoodComponent.Builder()
                                 .hunger(hunger)
@@ -116,9 +117,10 @@ public class ModItems {
         return item;
     }
 
-    private static Item consumableEffect(String name, int hunger, float saturation, StatusEffect effect, int duration, int amplifier, int maxCount) {
+    private static Item consumableEffect(String name, int hunger, float saturation, StatusEffect effect, int duration, int amplifier, int maxCount, Item remainder) {
         Item item = Registry.register(Registries.ITEM, new Identifier(MoreDelight.MOD_ID, name),
                 new ConsumableItem(new FabricItemSettings()
+                        .recipeRemainder(remainder)
                         .maxCount(maxCount)
                         .food(new FoodComponent.Builder()
                                 .hunger(hunger)
